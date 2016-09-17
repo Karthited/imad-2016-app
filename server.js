07ad4a65f2b33e519b0527364a151938621848fa
karthited <arulkarthikeyan91@gmail.com>
2016-09-17 13:18:26 +0530

    added server js file

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+app.use('/ui/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/ui/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/ui/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/ui/fonts', express.static(__dirname + '/node_modules/bootstrap/dist/fonts'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/img.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'img.jpg'));
 });
 
 
